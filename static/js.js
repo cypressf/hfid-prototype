5d0fb081e4bd463731c29bd25ae2c4a6aa761ca4
Cypress Frankenfeld <iamcypress@gmail.com>
2012-11-30 13:07:23 -0500

    check marks appear when individual sets saved
    It doesn't work as expected, all the time! Because we can't save individual sets, but instead try saving all sets in a form at the same time, there are some strange quirks. For example, if there's an error saving the form, we don't know if it's a problem with the individual set, or with all the sets as a whole. The alert box that pops up when a workout is saved more accurately represents the internal state of our app. One way to fix this would be to change the model so that a "set" is an object, or a table in the database. Each workout could contain multiple links to a different sets. This would enable us to create set objects, and save them to a workout, and do so individually as users fill out sets line by line, as opposed to trying to save the whole workout each time.

--- static/js.js
+++ static/js.js
@@ -20,7 +20,8 @@ $("#search input").keyup(search);
 $("#search input").click(search);
 $("#edit_goals").click(not_implemented);
 $("#add").click(not_implemented);
-// $("input[type=tel]").blur(check_for_submit);
+$("input[type=tel]:last-child").live('blur', check_for_submit);
+
 
 function not_implemented(){
     $("#edit_goals").off("click", not_implemented);
@@ -105,8 +106,10 @@ function expand_measurement(){
 
 // onblur of form element, check to see if it can be submitted
 // if so, submit it
-function check_for_submit() {
+function check_for_submit(e) {
     save_workout($(this).closest("li"));
+    $(this).closest(".set").removeClass("saved");
+    spinner_on($(this).closest(".set")[0]);
 }
 
 
@@ -146,6 +149,8 @@ function submit_workout(form) {
             window.setTimeout(function(){
                 $(document.getElementById("wo_saved_notif_div")).slideToggle();
             },1000)
+            spinner_off(form);
+            $(".set", $(form)).addClass("saved");
         }
     });
 }
@@ -210,3 +215,28 @@ function done_button(bool){
 }
 
 done_button(false);
+
+function spinner_off(element) {
+    $(".spinner", $(element)).remove();
+}
+
+function spinner_on(element){
+    var opts = {
+      lines: 9, // The number of lines to draw
+      length: 4, // The length of each line
+      width: 2, // The line thickness
+      radius: 4, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      color: '#000', // #rgb or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 0, // Top position relative to parent in px
+      left: 265 // Left position relative to parent in px
+    };
+    var spinner = new Spinner(opts).spin(element);
+}
